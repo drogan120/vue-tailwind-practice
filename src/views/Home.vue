d767a817e5c16ff22a7db34b76513999701889ee
drogan120 <alimahmudin.hp@gmail.com>
2020-12-04 22:41:08 +0700

    fixing home layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container mt-5">
-        <div class="flex">
-            <div class="flex-row mx-auto w-3/12 shadow-lg p-3 rounded-md mr-6 ml-6">
+        <div class="flex flex-wrap justify-space-between mx-auto">
+            <div class="w-2/12 shadow-lg p-3 rounded-md mr-6 ml-6">
                 <img src="../assets/logo.png" class="mx-auto" />
                 <p class="mt-2 mb-2 text-gray-600 text-left text-sm">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -18,7 +18,7 @@
                     </router-link>
                 </div>
             </div>
-             <div class="flex-row mx-auto w-3/12 shadow-lg p-3 rounded-md mr-6 ml-6">
+            <div class="w-2/12 shadow-lg p-3 rounded-md mr-6 ml-6">
                 <img src="../assets/logo.png" class="mx-auto" />
                 <p class="mt-2 mb-2 text-gray-600 text-left text-sm">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -35,7 +35,7 @@
                     </router-link>
                 </div>
             </div>
-            <div class="flex-row mx-auto w-3/12 shadow-lg p-3 rounded-md mr-6 ml-6">
+            <div class="w-2/12 shadow-lg p-3 rounded-md mr-6 ml-6">
                 <img src="../assets/logo.png" class="mx-auto" />
                 <p class="mt-2 mb-2 text-gray-600 text-left text-sm">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -52,7 +52,58 @@
                     </router-link>
                 </div>
             </div>
-             <div class="flex-row mx-auto w-3/12 shadow-lg p-3 rounded-md mr-6 ml-6">
+            <div class="w-2/12 shadow-lg p-3 rounded-md mr-6 ml-6">
+                <img src="../assets/logo.png" class="mx-auto" />
+                <p class="mt-2 mb-2 text-gray-600 text-left text-sm">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Veritatis expedita ad laudantium voluptates culpa
+                    necessitatibus, maiores reprehenderit omnis tempora
+                    corrupti?
+                </p>
+                <div>
+                    <router-link
+                        to="/twitter"
+                        class="rounded bg-blue-400
+                         hover:bg-indigo-600 p-2 text-gray-100 text-xs mb-2 mt-2"
+                        >twitter
+                    </router-link>
+                </div>
+            </div>
+             <div class="w-2/12 shadow-lg p-3 rounded-md mr-6 ml-6">
+                <img src="../assets/logo.png" class="mx-auto" />
+                <p class="mt-2 mb-2 text-gray-600 text-left text-sm">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Veritatis expedita ad laudantium voluptates culpa
+                    necessitatibus, maiores reprehenderit omnis tempora
+                    corrupti?
+                </p>
+                <div>
+                    <router-link
+                        to="/twitter"
+                        class="rounded bg-blue-400
+                         hover:bg-indigo-600 p-2 text-gray-100 text-xs mb-2 mt-2"
+                        >twitter
+                    </router-link>
+                </div>
+            </div>
+            <div class="w-2/12 shadow-lg p-3 rounded-md mr-6 ml-6">
+                <img src="../assets/logo.png" class="mx-auto" />
+                <p class="mt-2 mb-2 text-gray-600 text-left text-sm">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Veritatis expedita ad laudantium voluptates culpa
+                    necessitatibus, maiores reprehenderit omnis tempora
+                    corrupti?
+                </p>
+                <div>
+                    <router-link
+                        to="/twitter"
+                        class="rounded bg-blue-400
+                         hover:bg-indigo-600 p-2 text-gray-100 text-xs mb-2 mt-2"
+                        >twitter
+                    </router-link>
+                </div>
+            </div>
+             <div class="w-2/12 shadow-lg p-3 rounded-md mr-6 ml-6">
                 <img src="../assets/logo.png" class="mx-auto" />
                 <p class="mt-2 mb-2 text-gray-600 text-left text-sm">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
